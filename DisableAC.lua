ca055e3ca914908fda517e57c6ce60d4f3eee676
Retro <92770300+RetroHvH@users.noreply.github.com>
2021-10-18 22:04:06 -0600

    Fixed Sleep Looping After Enemy Attack

---
+++ DisableAC.lua
@@ -0,0 +1,16 @@
+local clock = os.clock
+function sleep(n)  -- seconds
+  local t0 = clock()
+  while clock() - t0 <= n do end
+end
+
+local function dacoDodge()
+    local enemies = EntityList.Enemies()
+
+    for i, ent in ipairs(enemies) do
+        local enemyDodge = Entity.IsDodging(ent)
+        if enemyDodge then
+            sleep(1)
+        end
+    end
+end
