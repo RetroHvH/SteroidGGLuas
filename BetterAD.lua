1fcff50f3eb81acd84a109e514f76f5d00ab53e6
Retro <92770300+RetroHvH@users.noreply.github.com>
2021-10-18 22:05:07 -0600

    Fixed Steroid Not Dodging && Added new conditions

---
+++ BetterAD.lua
@@ -0,0 +1,12 @@
+local function betterAutoDodge()
+    local player = EntityList.Local()
+
+    for i, ent in ipairs(EntityList.enemies()) do
+        local PredPos = Pred.TracePos(ent, 10)
+        local distX = math.abs(predPos.x - player.Pos().x)
+
+        if distX < 150 && enemy.IsAttacking then
+            Input.Cast(UserCmd.Dodge)
+        end
+    end
+end
